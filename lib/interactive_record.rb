20619fbe26769590a2b226ebd503f493dc15b223
Elle Faraday <faraday.elle@gmail.com>
2018-01-13 23:25:56 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,7 +58,7 @@ class InteractiveRecord
     FROM #{self.table_name}
     WHERE ? = ?
     SQL
-    DB[:conn].execute(sql, attributes.keys.first, attributes[attributes.keys[0]])
+    DB[:conn].execute(sql, attributes.keys.first.to_s, attributes[attributes.keys[0]].to_s)
   end
 
   def self.column_names
